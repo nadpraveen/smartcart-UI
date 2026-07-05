73d7db5991dbac9abfcef4a69b963f9cbd3d149a
nadpraveen <praveen.42000@gmail.com>
2026-07-05 19:01:29 +0530

    minor chnage in get accesstoken path

--- lib/api/auth.ts
+++ lib/api/auth.ts
@@ -14,5 +14,5 @@ export const authApi = {
     apiClient.post("/api/v1/auth/login", data),
 
   generateSecureToken: (data: { token: string }) =>
-    apiClient.post('/genSecureWhatsappToken', data),
+    apiClient.post('/users/genSecureWhatsappToken', data),
 };
