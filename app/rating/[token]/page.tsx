2fbff1fba0df31a369a2f4677baa98641b529645
KNiranjan-01 <NiranjanK@vsaastechnologies.com>
2026-07-14 16:48:58 +0530

    Implemented Query param using sequence
    Implemented Query param using sequence

--- app/rating/[token]/page.tsx
+++ app/rating/[token]/page.tsx
@@ -6,7 +6,7 @@ import { useStore } from "@/store/useStore";
 import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import SessionRedirect from "@/components/ui/sessionRedirect";
-import { getChannel } from "@/lib/utils/channel";
+// import { getChannel } from "@/lib/utils/channel";
 
 export default function RatingTokenPage() {
   const { token } = useParams();
@@ -25,8 +25,8 @@ export default function RatingTokenPage() {
 
         if (cancelled) return;
         setUserAfterAuth(data?.response);
-        const ch = getChannel();
-        router.replace(ch === "whatsapp" ? "/rating?channel=whatsapp" : "/rating");
+        // const ch = getChannel();
+        router.replace("/rating?ch=whatsapp");
       } catch (err) {
         if (cancelled) return;
 
